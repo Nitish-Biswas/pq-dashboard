f14338b0384dac6679491039c719e2b0ad7aff8c
Nitish Biswas <nitishbiswas066@gmail.com>
2026-01-06 17:33:07 +0530

    Add redirect from root to dashboard in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true, // true = 308 redirect (Cached by browser), false = 307 (Temporary)
+      },
+    ];
+  },
 };
+
 
 export default nextConfig;
